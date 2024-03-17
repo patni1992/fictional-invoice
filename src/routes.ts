600bc4509030fd494e982809db418fbe5ccd0085
Patrik <patriknilsson1992@gmail.com>
2024-03-17 15:20:24 +0700

    feat: Adds route to generate pdf-invoice

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import Router from "koa-router";
 import { Context } from "koa";
 import { getCarriers } from "./models/carrier";
+import { getParcelsByCarrierAndDateRange } from "./models/parcel";
+import { calculateTotalInvoice } from "./services/invoice";
+import { generateInvoicePdf } from "./services/invoicePdf";
 
 const router = new Router();
 
@@ -10,6 +13,27 @@ router.get("/", async (ctx: Context) => {
     title: "Generate Invoice",
     carriers: carriers,
   });
+});
+
+router.post("/invoice", async (ctx: Context) => {
+  try {
+    const { carrier, toDate, fromDate } = ctx.request.body;
+    const parcels = await getParcelsByCarrierAndDateRange(
+      carrier,
+      new Date(toDate),
+      new Date(fromDate)
+    );
+    const { detailedInvoices, total } = calculateTotalInvoice(parcels);
+    const pdfStream = generateInvoicePdf(detailedInvoices, total);
+
+    ctx.response.set("Content-Type", "application/pdf");
+    ctx.response.set("Content-Disposition", 'inline; filename="invoice.pdf"');
+    ctx.body = pdfStream;
+  } catch (error) {
+    console.error(error);
+    ctx.status = 500;
+    ctx.body = "Internal Server Error";
+  }
 });
 
 export default router;
